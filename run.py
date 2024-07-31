3735fc7cdfb51119afdc93c8f5624b9f88d695c4
taito001 <mooney.taito001@mymdc.net>
2024-07-31 22:39:35 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 from webssh.main import main
-
+import sys
+from tornado.options import define, options
 
 if __name__ == '__main__':
+    define("xsrf", default=False, help="Enable XSRF protection", type=bool)
+    define("xheaders", default=False, help="Enable X-headers", type=bool)
+    define("origin", default="*", help="Origin for CORS", type=str)
+    
+    sys.argv.extend(['--xsrf=False', '--xheaders=False', '--origin=*'])
+    options.parse_command_line()
+    
+    print("XSRF setting:", options.xsrf)  # 调试输出
     main()
